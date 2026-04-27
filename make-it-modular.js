7ce8ba3de77df60984efca7b93066492569f6176
Mariia1311 <prozirko.mar@e-u.edu.ua>
2026-04-27 13:01:26 +0300

    Create make-it-modular.js

---
+++ make-it-modular.js
@@ -0,0 +1,8 @@
+const filterFn = require('./mymodule.js');
+const dir = process.argv[2];
+const filterStr = process.argv[3];
+
+filterFn(dir, filterStr, (err, list) => {
+  if (err) return console.error('Error:', err);
+  list.forEach(file => console.log(file));
+});
