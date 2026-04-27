557b0ca2bb60bd6e326c9807ce3bfb035e85c989
Mariia1311 <prozirko.mar@e-u.edu.ua>
2026-04-27 13:01:14 +0300

    Create http-collect.js

---
+++ http-collect.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+http.get(process.argv[2], (response) => {
+  let result = '';
+  response.setEncoding('utf8');
+
+  response.on('data', (data) => {
+    result += data;
+  });
+
+  response.on('end', () => {
+    console.log(result.length);
+    console.log(result);
+  });
+
+  response.on('error', console.error);
+}).on('error', console.error);
