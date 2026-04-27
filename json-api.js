307d5b4462a48f2186e1976374b4c2e3750155cf
Mariia1311 <prozirko.mar@e-u.edu.ua>
2026-04-27 13:01:21 +0300

    Create json-api.js

---
+++ json-api.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  const url = new URL(req.url, 'http://example.com');
+  const time = new Date(url.searchParams.get('iso'));
+  let result;
+
+  if (url.pathname === '/api/parsetime') {
+    result = {
+      hour: time.getHours(),
+      minute: time.getMinutes(),
+      second: time.getSeconds()
+    };
+  } else if (url.pathname === '/api/unixtime') {
+    result = { unixtime: time.getTime() };
+  }
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(result));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+
+server.listen(Number(process.argv[2]));
